2bf43cd6aad9b4713c7d481a4967a12998fec530
Shadows_of_Fire <Bward7864@gmail.com>
2018-07-30 23:34:46 -0400

    i am the fast

---
+++ src/main/java/shadows/fastfurnace/FastFurnace.java
@@ -0,0 +1,39 @@
+package shadows.fastfurnace;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import shadows.fastfurnace.block.BlockFastFurnace;
+import shadows.fastfurnace.block.TileFastFurnace;
+
+@Mod(modid = FastFurnace.MODID, name = FastFurnace.MODNAME, version = FastFurnace.VERSION)
+public class FastFurnace {
+
+	public static final String MODID = "fastfurnace";
+	public static final String MODNAME = "FastFurnace";
+	public static final String VERSION = "1.0.0";
+
+	public static final Logger LOG = LogManager.getLogger(MODID);
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+		MinecraftForge.EVENT_BUS.register(this);
+		GameRegistry.registerTileEntity(TileFastFurnace.class, new ResourceLocation("minecraft", "furnace"));
+	}
+
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public void blockBois(Register<Block> e) {
+		e.getRegistry().registerAll(new BlockFastFurnace(false).setRegistryName("minecraft", "furnace"), new BlockFastFurnace(true).setRegistryName("minecraft", "lit_furnace"));
+	}
+
+}
---
+++ src/main/java/shadows/fastfurnace/block/BlockFastFurnace.java
@@ -0,0 +1,62 @@
+package shadows.fastfurnace.block;
+
+import java.util.Random;
+
+import net.minecraft.block.BlockFurnace;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.Item;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class BlockFastFurnace extends BlockFurnace {
+
+	public BlockFastFurnace(boolean burn) {
+		super(burn);
+		setHardness(3.5F);
+		setSoundType(SoundType.STONE);
+		setTranslationKey("furnace");
+		setCreativeTab(CreativeTabs.DECORATIONS);
+		if (burn) setLightLevel(0.875F);
+	}
+
+	static Item furnace;
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return furnace == null ? furnace = Item.getItemFromBlock(Blocks.FURNACE) : furnace;
+	}
+
+	/*
+	@Override
+	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+	}
+	
+	@Override
+	public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
+		return getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+	}
+	*/
+
+	@Override
+	public void breakBlock(World world, BlockPos pos, IBlockState oldState) {
+		IBlockState state = world.getBlockState(pos);
+		if (state.getBlock() != Blocks.FURNACE && state.getBlock() != Blocks.LIT_FURNACE) {
+			TileEntity tileentity = world.getTileEntity(pos);
+			if (tileentity instanceof TileFastFurnace) {
+				InventoryHelper.dropInventoryItems(world, pos, (TileFastFurnace) tileentity);
+				world.updateComparatorOutputLevel(pos, this);
+			}
+			world.removeTileEntity(pos);
+		}
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World world, int meta) {
+		return new TileFastFurnace();
+	}
+}
