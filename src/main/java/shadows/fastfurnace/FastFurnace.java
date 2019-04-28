cb1ae1daca97a9a2bb1bc2fa10895a121ede2236
Shadows_of_Fire <Bward7864@gmail.com>
2019-04-28 13:09:44 -0400

    bump ver

--- src/main/java/shadows/fastfurnace/FastFurnace.java
+++ src/main/java/shadows/fastfurnace/FastFurnace.java
@@ -25,7 +25,7 @@ public class FastFurnace {
 
 	public static final String MODID = "fastfurnace";
 	public static final String MODNAME = "FastFurnace";
-	public static final String VERSION = "1.2.3";
+	public static final String VERSION = "1.3.0";
 
 	public static final Logger LOG = LogManager.getLogger(MODID);
 
