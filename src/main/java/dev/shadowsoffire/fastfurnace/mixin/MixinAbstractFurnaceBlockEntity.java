1775649baa2c1adecd4bf6aa5f83c086eab2f03d
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-09-24 01:05:19 -0700

    Port to 1.21.1

--- src/main/java/dev/shadowsoffire/fastfurnace/mixin/MixinAbstractFurnaceBlockEntity.java
+++ src/main/java/dev/shadowsoffire/fastfurnace/mixin/MixinAbstractFurnaceBlockEntity.java
@@ -2,6 +2,7 @@ package dev.shadowsoffire.fastfurnace.mixin;
 
 import java.util.Optional;
 
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -11,18 +12,20 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeInput;
 import net.minecraft.world.item.crafting.RecipeManager.CachedCheck;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.SingleRecipeInput;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
-@Mixin(AbstractFurnaceBlockEntity.class)
+@Mixin(value = AbstractFurnaceBlockEntity.class, remap = false)
 public abstract class MixinAbstractFurnaceBlockEntity extends BaseContainerBlockEntity {
 
     protected MixinAbstractFurnaceBlockEntity(BlockEntityType<?> p_155076_, BlockPos p_155077_, BlockState p_155078_) {
@@ -30,36 +33,49 @@ public abstract class MixinAbstractFurnaceBlockEntity extends BaseContainerBlock
     }
 
     @Unique
-    protected AbstractCookingRecipe curRecipe;
+    @Nullable
+    protected RecipeHolder<AbstractCookingRecipe> curRecipe;
+
     @Unique
     protected ItemStack failedMatch = ItemStack.EMPTY;
 
     @Shadow
     protected RecipeType<? extends AbstractCookingRecipe> recipeType;
 
+    @Nullable
     @SuppressWarnings("unchecked")
-    protected AbstractCookingRecipe getRecipe() {
+    protected RecipeHolder<AbstractCookingRecipe> getRecipe() {
         ItemStack input = this.getItem(0);
-        if (input.isEmpty() || ItemStack.isSameItemSameTags(this.failedMatch, input)) return null;
-        if (this.curRecipe != null && this.curRecipe.matches(this, this.level)) return this.curRecipe;
+        if (input.isEmpty() || ItemStack.isSameItemSameComponents(this.failedMatch, input)) {
+            return null;
+        }
+
+        if (this.curRecipe != null && this.curRecipe.value().matches(new SingleRecipeInput(input), this.level)) {
+            return this.curRecipe;
+        }
         else {
-            AbstractCookingRecipe rec = this.level.getRecipeManager().getRecipeFor((RecipeType<AbstractCookingRecipe>) this.recipeType, this, this.level).orElse(null);
-            if (rec == null) this.failedMatch = input.copy();
-            else this.failedMatch = ItemStack.EMPTY;
+            RecipeHolder<AbstractCookingRecipe> rec = this.level.getRecipeManager().getRecipeFor((RecipeType<AbstractCookingRecipe>) this.recipeType, new SingleRecipeInput(input), this.level).orElse(null);
+
+            if (rec == null) {
+                this.failedMatch = input.copy();
+            }
+            else {
+                this.failedMatch = ItemStack.EMPTY;
+            }
+
             return this.curRecipe = rec;
         }
     }
 
-    // Has to be public due to possiblity of AT application to this method.
     @Inject(at = @At("HEAD"), method = "getTotalCookTime", cancellable = true)
     private static void fastfurnace_useFFRecipeCache(Level pLevel, AbstractFurnaceBlockEntity pBlockEntity, CallbackInfoReturnable<Integer> cir) {
-        AbstractCookingRecipe rec = ((MixinAbstractFurnaceBlockEntity) (Object) pBlockEntity).getRecipe();
-        cir.setReturnValue(rec == null ? 200 : rec.getCookingTime());
+        RecipeHolder<AbstractCookingRecipe> rec = ((MixinAbstractFurnaceBlockEntity) (Object) pBlockEntity).getRecipe();
+        cir.setReturnValue(rec == null ? 200 : rec.value().getCookingTime());
     }
 
-    @Redirect(method = "serverTick(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/block/entity/AbstractFurnaceBlockEntity;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/crafting/RecipeManager$CachedCheck;getRecipeFor(Lnet/minecraft/world/Container;Lnet/minecraft/world/level/Level;)Ljava/util/Optional;"), require = 1)
-    private static Optional<AbstractCookingRecipe> getRecipe(CachedCheck<?, ?> c, Container inv, Level level) {
-        return Optional.ofNullable(((MixinAbstractFurnaceBlockEntity) inv).getRecipe());
+    @Redirect(method = "serverTick(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/block/entity/AbstractFurnaceBlockEntity;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/crafting/RecipeManager$CachedCheck;getRecipeFor(Lnet/minecraft/world/item/crafting/RecipeInput;Lnet/minecraft/world/level/Level;)Ljava/util/Optional;"), require = 1)
+    private static Optional<RecipeHolder<?>> getRecipe(CachedCheck<?, ?> c, RecipeInput inv, Level level, Level levelAgain, BlockPos pos, BlockState state, AbstractFurnaceBlockEntity blockEntity) {
+        return Optional.ofNullable(((MixinAbstractFurnaceBlockEntity) (Object) blockEntity).getRecipe());
     }
 
 }
